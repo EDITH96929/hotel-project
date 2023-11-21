8ba8fb67443f79251710de9867559e5b2ec09dd0
EDITH96929 <130074679+EDITH96929@users.noreply.github.com>
2023-11-21 13:38:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("welcome to rg homes ");
+alert("welcome to rg homes");
+
+// datatype in js
+let a =5; 
+let b= sunil;
+let c = true;
+let d = undefined;
+console.log("a,b.c,d");
+
+console.log("typeof a,typeof b,typeof c,typeof d");
